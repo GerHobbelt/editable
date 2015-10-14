31edecc2b19cf739d9aa35c5678ec67e001aed27
Ger Hobbelt <ger@hobbelt.com>
2015-10-14 04:00:59 +0200

    mixing in patches from master branch

--- build/build.js
+++ build/build.js
@@ -1076,7 +1076,7 @@ Editable.prototype.onchange = function(e){
   var self = this;
   autosave(function(){
     var buf = new String(self.toString());
-    buf.at = position(el);
+    buf.at = position(self.el);
     self.history.add(buf);
     return self.emit('change', e);
   });
